1fedc4af2d64bebdc98506c35acf89730692208b
helloSylvia <wsrsdjd@sina.com>
2017-04-27 14:39:30 +0800

    项目修改

--- jasmine/spec/feedreader.js
+++ jasmine/spec/feedreader.js
@@ -11,13 +11,23 @@ $(function() {
      * 都是关于 Rss 源的定义的，也就是应用中的 allFeeds 变量。
     */
     describe('RSS Feeds', function() {
+    	
+    	// 统一测试 是否定义且不为空
+    	function isDefAndNotNull(str){
+    		
+    		expect(str).toBeDefined();
+    		expect(str).not.toBeNull();
+    	}
+    	// url 匹配正则
+    	var regularExpressionUrl = /^((ht|f)tps?):\/\/([\w\-]+(\.[\w\-]+)*\/)*[\w\-]+(\.[\w\-]+)*\/?(\?([\w\-\.,@?^=%&:\/~\+#]*)+)?/;
+    	
         /* 这是我们的第一个测试 - 它用来保证 allFeeds 变量被定义了而且
          * 不是空的。在你开始做这个项目剩下的工作之前最好实验一下这个测试
          * 比如你把 app.js 里面的 allFeeds 变量变成一个空的数组然后刷新
          * 页面看看会发生什么。
         */
         it('are defined', function() {
-            expect(allFeeds).toBeDefined();
+        	expect(allFeeds).toBeDefined();
             expect(allFeeds.length).not.toBe(0);
         });
 
@@ -28,10 +38,10 @@ $(function() {
 		it('allFeeds 对象里面的所有的源保证有链接字段而且链接不是空的',function() {
 			
 			for(var i = 0; i < allFeeds.length; i++ ){
-				// 判断url 字段是否被定义 
-				expect(allFeeds[i].url).toBeDefined();
-				// 判断url 字段不是空
-				expect(allFeeds[i].url).not.toBeNull();
+				// 判断url 字段是否定义且不为空
+				isDefAndNotNull(allFeeds[i].url);
+				// 验证url格式
+				expect(allFeeds[i].url).toMatch(regularExpressionUrl);
 			}
 		});
 
@@ -41,8 +51,8 @@ $(function() {
         
         it('allFeeds 对象里面的所有的源保证有名字字段而且不是空的',function() {
         	for(var i = 0; i < allFeeds.length; i++){
-        		expect(allFeeds[i].name).toBeDefined();
-        		expect(allFeeds[i].name).not.toBeNull();
+        		// 判断name 字段是否定义且不为空
+        		isDefAndNotNull(allFeeds[i].name);
         	}
         });
     });
@@ -53,8 +63,14 @@ $(function() {
    		// 获取body的className
    		var bodyClass,
    			menuIcon;
+   			
+   		// 统一判断 指定对象是否有指定class
+   		function hasClass(obj,str){
+   			return obj.hasClass(str);
+   		}
+   		
 		beforeEach(function(){
-			bodyClass = $('body')[0];
+			body = $('body');
 			menuIcon = $('.menu-icon-link');
 		});
 
@@ -63,8 +79,8 @@ $(function() {
          * 来搞清楚我们是怎么实现隐藏/展示菜单元素的。
          */
         it('菜单元素默认是隐藏的',function() {
-        	// body 中的class 控制隐藏显示
-        	expect(bodyClass.className).toEqual('menu-hidden');
+        	// body含有'menu-hidden' class 是否为真
+        	expect(hasClass(body,'menu-hidden')).toBeTruthy();
         });
 
          /* TODO:
@@ -76,15 +92,15 @@ $(function() {
          	// 设置点击事件
          	iconClick = function() {
          		menuIcon.click();
-         	}
+         	};
          	// 第一次点击
          	iconClick();
-//       	console.log(bodyClass.className);
-         	expect(bodyClass.className).toEqual('');
+//       	body含有'menu-hidden' class 是否为假
+         	expect(hasClass(body,'menu-hidden')).not.toBeTruthy();
          	// 第二次点击
          	iconClick();
-//       	console.log(bodyClass.className);
-         	expect(bodyClass.className).toEqual('menu-hidden');
+//       	body含有'menu-hidden' class 是否为真
+         	expect(hasClass(body,'menu-hidden')).toBeTruthy();
          	
          });
 
@@ -98,23 +114,27 @@ $(function() {
          * 记住 loadFeed() 函数是异步的所以这个而是应该使用 Jasmine 的 beforeEach
          * 和异步的 done() 函数。
          */
+	// 获取默认超时时间值
+    var defaultOutTime = jasmine.DEFAULT_TIMEOUT_INTERVAL;    	
 	describe('Initial Entries',function() {
 		
 		beforeEach(function(done){
-			// 在回到函数中done
-			loadFeed(0,function(){
-				// cb 返回后done测试
-				done();
-			});
+			// 设置该suite的超时时间
+			jasmine.DEFAULT_TIMEOUT_INTERVAL = 10000;
+			loadFeed(0,done);
 		});
 		
-		it('loadFeed 函数被调用而且工作正常',function(done) {
+		it('loadFeed 函数被调用而且工作正常',function() {
 			var entrys = $('.feed .entry');
 			// 返回的数据条数比0大，就说明成功
 			expect(entrys.length).toBeGreaterThan(0);
-			done();
+		});
 		
-		},10000);
+		// 每个spec 后恢复默认，不影响其他功能
+		afterEach(function() {
+            jasmine.DEFAULT_TIMEOUT_INTERVAL = defaultOutTime;
+        });
+		
 		
 	});
 
@@ -125,9 +145,12 @@ $(function() {
          * 记住，loadFeed() 函数是异步的。
          */
     describe('New Feed Selection',function(){
+    	
     	var feedOne,
     	    feedTwo = '';
 		beforeEach(function(done){
+			// 设置该suite的超时时间
+			jasmine.DEFAULT_TIMEOUT_INTERVAL = 10000;
 			// 初始化
 			loadFeed(0, function() {
 		 		// 取初始化时.feed中的内容
@@ -142,12 +165,14 @@ $(function() {
 			});
 		});
 		
-		it('loadFeed 函数加载一个新源的时候内容会真的改变',function(done) {
+		it('loadFeed 函数加载一个新源的时候内容会真的改变',function() {
 			// 第二次内容与第一次内容不等，即成功
 			expect(feedTwo).not.toEqual(feedOne);
-			done();
-		// 把时间稍微定的大一点，以免因请求google api 超时不通过
-		},10000);
-    	
+		});
+		
+    	// 每个spec 后恢复默认，不影响其他功能
+    	afterEach(function() {
+            jasmine.DEFAULT_TIMEOUT_INTERVAL = defaultOutTime;
+        });
     });
 }());
